47bcb93cf97e14eb3a79a11a65d7150726d6c818
sergeveli <70241892+sergeveli@users.noreply.github.com>
2023-04-12 12:55:01 -0400

    indents in prompts.py fixed

--- Employee Management System/_prompts.py
+++ Employee Management System/_prompts.py
@@ -1,18 +1,9 @@
-"""This module contains reusable code for prompting user to input employee details.
-        Functions: get_employee_details():
-        Prompts the user to input employee details including first name, last name, DOB, age,
-        date of employment, salary, and department. Returns a tuple containing all the details.
-        """
+"""Function to prompt user input"""
+def get_employee_details(first_name, last_name, age, dob, emp_date, department, salary):
+    first_name = input("Enter Employee First Name: ")
+    last_name = input("Enter Employee Last Name:")
+    age = int(input("Enter Employee Age: "))
+    emp_date = input("Enter Employee Date of Employment (YYYY-MM-DD): ")
+    department = input("Enter Employee Department: ")
+    salary = input("Enter Employee Salary: ")
 
-
-def get_employee_details():
-        """Function to collect employee info """
-        first_name = input("Enter Employee First Name: ")
-        last_name = input("Enter Employee Last Name: ")
-        dob = input("Enter Employee Date of Birth (YYYY-MM-DD): ")
-        age = int(input("Enter Employee Age: "))
-        emp_date = input("Enter Employee Date of Employment (YYYY-MM-DD): ")
-        salary = int(input("Enter Employee Salary: "))
-        department = input("Enter Employee Department: ")
-
-        return first_name, last_name, dob, age, emp_date, salary, department
