0aba03b70d1b294a9fc9ec73f6c3147c9fb895fe
David <adenir11@uwindsor.ca>
2023-04-13 10:36:18 -0400

    committng adding remove method

--- Employee Management System/main.py
+++ Employee Management System/main.py
@@ -3,26 +3,33 @@ import manager
 system_lib = {
     1: "Display",
     2: "Add",
-    3: "Update"
+    3: "Update",
+    4: "Remove"
 }
 
 while True:
     print(f"-----------------------------\nSystem Library:\n1:  {system_lib[1]}\n2: {system_lib[2]}\n3: {system_lib[3]}\n")
     ID_type = input("\nWhat would you like to Access? Input system ID, or type 'exit' to exit:\t").capitalize()
 
-    if ID_type == '1':
+    if ID_type == system_lib[1]:
        #Displaying list of current employees
         manager.displayEmployeeList()
         ID_type = None
 
-    elif ID_type == '2':
+    elif ID_type == system_lib[2]:
         #Adding employees
         employee()
         ID_type = None
 
-    elif ID_type == '3':
+    elif ID_type == system_lib[3]:
         # Update method
         manager.updateEmployeeData()
+        ID_type = None
+
+    elif ID_type == system_lib[4]:
+        #Remove method
+        manager.removeEmployee()
+        ID_type = None
 
     elif ID_type == 'Exit':
         break   
