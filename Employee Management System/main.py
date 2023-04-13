6ac27b3cea21ff760f891f2be0d5cdba1e83a5fb
Dusty-Willow <47825254+Dusty-Willow@users.noreply.github.com>
2023-04-13 16:19:50 -0400

    Added number navigation

--- Employee Management System/main.py
+++ Employee Management System/main.py
@@ -7,7 +7,8 @@ system_lib = {
     2: "Add",
     3: "Update",
     4: "Remove",
-    5: "Filter"
+    5: "Filter",
+    6: "Exit"
 }
 
 try:
@@ -18,34 +19,34 @@ except:
 
 while True:
     print(f"-----------------------------\nSystem Commands:\n1: {system_lib[1]}\n2: {system_lib[2]}\n3: {system_lib[3]}\n4: {system_lib[4]}\n5: {system_lib[5]}\n6: Exit\n")
-    ID_type = input("\nWhat would you like to Access? Input system command, or type 'exit' to exit: ").title()
+    ID_type = input("\nWhat would you like to Access? Input system command: ").title()
 
-    if ID_type == system_lib[1]:
+    if (ID_type == system_lib[1] or ID_type == "1"):
        #Displaying list of current employees
         manager.displayEmployeeList()
         ID_type = None
 
-    elif ID_type == system_lib[2]:
+    elif (ID_type == system_lib[2] or ID_type == "2"):
         #Adding employees
         manager.addEmployee()
         ID_type = None
 
-    elif ID_type == system_lib[3]:
+    elif (ID_type == system_lib[3] or ID_type == "3"):
         # Update method
         manager.updateEmployeeData()
         ID_type = None
 
-    elif ID_type == system_lib[4]:
+    elif (ID_type == system_lib[4] or ID_type == "4"):
         #Remove method
         manager.removeEmployee()
         ID_type = None
 
-    elif ID_type == system_lib[5]:
+    elif (ID_type == system_lib[5] or ID_type == "5"):
         # Filter method
         manager.filterEmployee()
         ID_type = None
 
-    elif ID_type == 'Exit':
+    elif (ID_type == system_lib[6] or ID_type == "6"):
         break
 
     else:
